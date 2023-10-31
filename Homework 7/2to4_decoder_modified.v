cef87e69162b7f24535d5fdac5c4a2a363571af1
Gabe DiMartino <Macbee280@gmail.com>
2023-10-31 10:58:21 -0700

    Update 2to4_decoder_modified.v

--- Homework 7/2to4_decoder_modified.v
+++ Homework 7/2to4_decoder_modified.v
@@ -1,14 +1,16 @@
 module 2to4_decoder (W, En, Y);
     input [1:0] W;
     input En;
-    output reg [0:3] Y;
+    output reg [3:0] Y; //Flip 0:3
     integer k;
 
   always @(W, En)
-    for (k = 0; k <= 3; k = k + 1) begin
+    begin // Add begin
+        for (k = 0; k <= 3; k = k + 1) begin // Begin
       if (W == k)
         Y[k] = En;
       else
         Y[k] = 0; //Fixed to set all values not equal to K as 0. Also made sure all info was inside the always block
-    end
+    end // Add End
+    end // Add End
 endmodule
